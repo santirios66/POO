3ee3084dd32e706f6bbf535b82285d772bbfdd77
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-08-06 21:00:15 +0000

    creacion de la varibale x

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,11 @@
 public class  HolaMundo {
-
+    
     public static void main(String[] args) {
 
         System.out.println("mi primer hola mundo en JAVAA!!!!!");
+   
+        //creacion de la variable x de tipo entero
+        int x = 2;
 
 
     }
