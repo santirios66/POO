42815a71c8dcf0e21668feb7c7fec6cad04f3031
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-08-13 22:10:51 +0000

    calculos de clase de 13/08/25

--- Grupo2/clase2/Expresiones.java
+++ Grupo2/clase2/Expresiones.java
@@ -4,7 +4,13 @@ public class Expresiones {
 
     public static void main(String[] arg) {
 
-        System.out.println(B * A B ^ 2 / 4 * C); 
+        int A = 4;
+        int B = 5;
+        int C = 1;
+
+        System.out.println((B * A - Math.pow(B, 2)/ 4 * C)); 
+        System.out.println((A * B) / Math.pow(3, 2));
+        System.out.println((((B + C) / 2 * A + 10) * 3 * B) - 6);
 
      
     }
