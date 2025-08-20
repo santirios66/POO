89b1c6ab867dec360e5e5be0b09662637b89d365
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-08-20 19:45:07 +0000

    cambios

--- Grupo2/clase2/Expresiones_trabajo.java
+++ Grupo2/clase2/Expresiones_trabajo.java
@@ -1,13 +1,13 @@
 public class Expresiones_trabajo {
     public static void main(String[] args) {
         double d1, d2, d3;
-        double t0, t1, t2, t3; // Añadí t1 que se usa en el print
+        double t1, t2, t3; 
         double l1, l2, l3;
-        double predcultro, maskdargakg, largo, ancho, alto;
-        double p1, p2, p3, deprechorkm, v;
+        double precioLitro, masaCargaKg, largo, ancho, alto;
+        double p1, p2, p3, deprecPorKm, v;
         
         System.out.println("d1, d2, d3, t1, t2, t3, l1, l2, l3, "
-                + "predcultro, maskdargakg, largo, ancho, alto, "
-                + "p1, p2, p3, deprechorkm, v (sin valor asignado)");
+                + "precioLitro, masaCargaKg, largo, ancho, alto, "
+                + "p1, p2, p3, deprecPorKm, v (sin valor asignado)");
     }
 }
