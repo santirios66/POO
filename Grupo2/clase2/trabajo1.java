302d56bf0995dd50c757350fbba662280c4ea639
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-08-20 19:56:40 +0000

    cambhio3

--- Grupo2/clase2/trabajo1.java
+++ Grupo2/clase2/trabajo1.java
@@ -1,3 +1,32 @@
 public class trabajo1 {
+    public static void main(String[] args) {
+        //punto 1 
+        double d1, d2, d3;
+        double t1, t2, t3; 
+        double l1, l2, l3;
+        double precioLitro, masaCargaKg, largo, ancho, alto;
+        double p1, p2, p3, deprecPorKm, v;
+        
+        System.out.println("d1, d2, d3, t1, t2, t3, l1, l2, l3, "
+                + "precioLitro, masaCargaKg, largo, ancho, alto, "
+                + "p1, p2, p3, deprecPorKm, v (sin valor asignado)");
+        
+        //punto 2 
+        double v1 = d1 / t1;
+        double v2 = d2 / t2;
+        double v3 = d3 / t3;
+        double vprom = (d1 * v1 + d2 * v2 + d3 * v3) / (d1 + d2 + d3);
+        double kml1 = d1 / l1 ;
+        double kml2 = d2 / l2 ;
+        double kml3 = d3 / l3 ;
+        double kmlTotal = (d1 + d2 + d3)/(l1 + l2 + l3);
+
+        //punto 3 costos directos
+
     
+    }
+
+    }
 }
+
+
