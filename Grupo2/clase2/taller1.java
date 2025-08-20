a092e4e67fd87ba7e8f36802334cc8cc2373d76b
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-08-20 19:41:49 +0000

    trabjo 1

---
+++ Grupo2/clase2/taller1.java
@@ -0,0 +1,13 @@
+public class Expresiones_trabajo {
+    public static void main(String[] args) {
+        double d1, d2, d3;
+        double t0, t1, t2, t3; // Añadí t1 que se usa en el print
+        double l1, l2, l3;
+        double predcultro, maskdargakg, largo, ancho, alto;
+        double p1, p2, p3, deprechorkm, v;
+        
+        System.out.println("d1, d2, d3, t1, t2, t3, l1, l2, l3, "
+                + "predcultro, maskdargakg, largo, ancho, alto, "
+                + "p1, p2, p3, deprechorkm, v (sin valor asignado)");
+    }
+}
