6be9f26eeb3fcf86474f1bc098d607f505615f6b
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-09-17 21:04:22 +0000

    Update primeros atributos y metodos

---
+++ Grupo2/Clase7/P.java
@@ -0,0 +1,5 @@
+package Grupo2.clase7;
+
+public class Person {
+    
+}
---
+++ Grupo2/Clase7/Person.java
@@ -0,0 +1,25 @@
+package Grupo2.Clase7;
+
+public class Person {
+    
+
+    // Los Atributos siempre deben de ser privados. 
+
+    // Atributos.
+
+    private int Identificacion;
+    private String Nombre;
+    private String Apellidos;
+    private int Edad;
+    private double estatura;
+    private double peso;
+    
+    
+    // Los metodos son siempre publicos.
+    //Metodos 
+
+    public static void Caminar() {}
+    public static void Jugar() {}
+    public static void Comer(){}
+    public static void Estudiar(){}
+}
