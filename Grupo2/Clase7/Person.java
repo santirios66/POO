ca40c61cb39dc47f4ff05be3e06eff80e613b6e3
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-09-17 21:53:40 +0000

    Update

--- Grupo2/Clase7/Person.java
+++ Grupo2/Clase7/Person.java
@@ -1,9 +1,8 @@
 package Grupo2.Clase7;
 
 public class Person {
-    
 
-    // Los Atributos siempre deben de ser privados. 
+    // Los Atributos siempre deben de ser privados.
 
     // Atributos.
 
@@ -11,15 +10,29 @@ public class Person {
     private String Nombre;
     private String Apellidos;
     private int Edad;
-    private double estatura;
+    private double Estatura;
     private double peso;
-    
-    
+
+    // Metodo constructor de la clase se reconcoe por que tine el mismo nombre de la clase.
+    //El constructor de la clase me permite inicializar laclase 
+    // la palabra reservada "this " se utiliza para autoreferenciar el atributo de la clase.
+    public Person(int Identificacion, String Nombre, String Apellidos,int Edad,double Estatura, double peso){
+        this.Identificacion = Identificacion;
+        this.Nombre = Nombre;
+        this.Apellidos = Apellidos;
+        this.Edad = Edad;
+        this.Estatura = Estatura;
+        this.peso = peso;
+    }
     // Los metodos son siempre publicos.
-    //Metodos 
+    // Metodos.
 
     public static void Caminar() {}
+
     public static void Jugar() {}
-    public static void Comer(){}
-    public static void Estudiar(){}
+
+    public static void Comer() {}
+
+    public static void Estudiar() {}
+ 
 }
