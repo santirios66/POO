0075a4187a474574b507de00798754021c1a3da5
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-10-01 22:51:27 +0000

    update tarea

--- Grupo2/clase9/EjecutarOpeMatGrupo2.java
+++ Grupo2/clase9/EjecutarOpeMatGrupo2.java
@@ -15,6 +15,19 @@ public class EjecutarOpeMatGrupo2 {
         System.out.println("digite el segundo  numero...");
         int b = sc.nextInt();
 
+        System.out.println("Digite el numero deseado para las operaciones : ");
+        int d = sc.nextInt();
+        
+        // Menú de opciones
+        System.out.println("Elige la operacion que desea realizar:");
+        System.out.println("1: elevar al cuadrado");
+        System.out.println("2: elevar al cubo");
+        System.out.println("3: elevar ala n");
+        System.out.println("4: raiz cuadrada");
+        System.out.println("5: raiz al cubo");
+        System.out.println("6: raiz de n");
+        int c = sc.nextInt();
+
         /*
          * para leer enteros : nextInt() o nextLong()
          * para leer reales : nextDouble() o nextFloat()
@@ -22,13 +35,54 @@ public class EjecutarOpeMatGrupo2 {
          * para leer booleanos : nextBoolean()
          */
 
-        OperacionesMatematicas objOp = new OperacionesMatematicas(a, b);
+        OperacionesMatematicas objOp = new OperacionesMatematicas(a, b, c, d);
 
+        // Operaciones básicas
         System.out.println("El resultado de la suma es: " + objOp.sumar());
-        System.out.println("el resultado de la resta: " + objOp.resta());
+        System.out.println("El resultado de la resta: " + objOp.resta());
         System.out.println("El resultado de la multiplicacion es: " + objOp.multi());
         System.out.println("El resultado de la division es: " + objOp.dividir());
 
+        // Operaciones de potencia y raíz con switch
+        switch (c) {
+            case 1:
+                System.out.println("Su numero elevado al cuadrado es : " + objOp.elevado_cuadrado());
+                break;
+            case 2:
+                System.out.println("Su numero elevado al cubo es : " + objOp.elevado_cubo());
+                break;
+            case 3:
+                System.out.println("Digite el numero n a elevar su digitado anteriormente....");
+                int n = sc.nextInt();
+                System.out.println("Su numero " + d + " elevado a la " + n + " es : " + objOp.elevadoN(n));
+                break;
+            case 4:
+                System.out.println("Resultado: " + objOp.raizCuadrada());
+                break;
+            case 5:
+                System.out.println("Resultado: " + objOp.raizCubo());
+                break;
+            case 6:
+                System.out.print("Digite el valor de n para la raíz: ");
+                int raiz = sc.nextInt();
+                System.out.println("La raiz " + raiz + " de " + d + " es: " + objOp.raizN(raiz));
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
+
         sc.close();
+
+        /*
+         * Tarea: implementar lo metodos
+         * -elevar al cuadrado elevar al cubo
+         * -elevar a la n
+         * - sacar la raiz cuadrada
+         * - sacar la raiz cubica
+         * -sacar la raiz n
+         * -todo lo anterior con un menu de opcion.
+         */
+
     }
 }
--- Grupo2/clase9/OperacionesMatematicas.java
+++ Grupo2/clase9/OperacionesMatematicas.java
@@ -5,25 +5,32 @@ public class OperacionesMatematicas {
     // Atributos.
     private int n1;
     private int n2;
+    private int options_operation;
+    private double n3;
 
     // Constructor de la clase. permite inicializar la clase.
     // Tiene el mismo nombre de la clase.
-    /*irma del emtodo constructor.
-    visibilidad : publico
-    nombre : OperacionesMatematicas.
-    Cuantos parametros:2
-    De que tipo son los parametros.
-        Parametro 1 tipo ENTERO.
-        Parametro 2 tipo ENTERO.
+    /*
+        Firma del metodo constructor:
+        visibilidad : publico
+        nombre : OperacionesMatematicas.
+        Cuantos parametros:4
+        De que tipo son los parametros.
+            Parametro 1 tipo ENTERO.
+            Parametro 2 tipo ENTERO.
+            Parametro 3 tipo ENTERO.
+            Parametro 4 tipo DOUBLE.
     */ 
-    public OperacionesMatematicas(int n1, int n2) {
+    public OperacionesMatematicas(int n1, int n2, int options_operation, double n3) {
 
         this.n1 = n1; // this signifIca una auto referencia al atributo de la clase.
         this.n2 = n2;
+        this.options_operation = options_operation;
+        this.n3 = n3;
 
     }
 
-    // Metodos.
+    // Metodos de operaciones básicas.
     public int sumar(){
         return n1 + n2;
     }
@@ -41,10 +48,34 @@ public class OperacionesMatematicas {
         if (n2 == 0){
             result = 0;
         }else {
-            result =  n1 / n2;
+            result =  (double) n1 / n2; // conversión para que no se pierdan decimales
         }
         return result;
     }
 
+    // Metodos de potencias
+    public double elevado_cuadrado(){
+        return Math.pow(n3, 2);
+    }
     
+    public double elevado_cubo(){
+        return Math.pow(n3, 3);
+    }
+
+    public double elevadoN(double n){
+        return Math.pow(n3, n);
+    }
+
+    // Metodos de raíces
+    public double raizCuadrada(){
+        return Math.sqrt(n3);
+    }
+
+    public double raizCubo(){
+        return Math.cbrt(n3);
+    }
+
+    public double raizN(double n){
+        return Math.pow(n3, 1.0 / n);
+    }
 }
