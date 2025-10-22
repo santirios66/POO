c54bff0392638570aeaa0ad9ff42cbb7d5787440
santirios66 <santiago.patinorio@campusucc.edu.co>
2025-10-22 20:35:45 +0000

    Herencia Update

---
+++ Grupo2/clase12/Ejemplo1/Animal.java
@@ -0,0 +1,8 @@
+package Grupo2.clase12.Ejemplo1;
+
+public class Animal {
+
+    public void hacerSonido() {
+        System.out.println("Sonido Generico");
+    }
+}
---
+++ Grupo2/clase12/Ejemplo1/Perro.java
@@ -0,0 +1,8 @@
+package Grupo2.clase12.Ejemplo1;
+
+public class Perro extends Animal {
+
+    public void hacerSonido() {
+        System.out.println("Guau!");
+    }
+}
